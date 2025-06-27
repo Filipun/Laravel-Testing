9d605c354918e7a265e483d4dd1e16d5370da59e
Filip Polomski <filipun@Filip--MacBook-Air.local>
2025-06-27 16:03:26 +0200

    initial commin

---
+++ app/Console/Commands/pepa.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class pepa extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:pepa';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
